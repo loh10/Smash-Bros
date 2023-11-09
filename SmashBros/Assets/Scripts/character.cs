e7c92d96d6abe6f00013c68dc8f0451c7e7b2dda
WhaleMainStudio <148849020+WhaleMainStudio@users.noreply.github.com>
2023-11-09 20:16:00 +0100

    Ajout système de saut et inputs manette
    - Saut simple
    - Double saut
    - Saut simple prolongé
    - Double saut prolongé
    
    Inputs map de la manette (avec 2 actions debug pour le moment)

--- SmashBros/Assets/Scripts/character.cs
+++ SmashBros/Assets/Scripts/character.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+//using System.Numerics;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class character : MonoBehaviour
 {
@@ -8,24 +11,28 @@ public class character : MonoBehaviour
     private Rigidbody2D rb;
     private int powerOffset = 1;
 
+   // private bool checkIfGrounded;
+    private int remainingJumps;
+    private bool isJumping = false;
+    private float currentJumpTime = 0;
+
+    private enum characterState
+{
+    Void,
+    Jumping,
+    Attacking,
+    Stun,
+    Ejection
+}
+
     void Start()
     {
         _expulsionPercentage = GetComponent<expulsionPercentage>();
+        remainingJumps = 2;
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Vector3 v = new Vector3(100, 100, 0);
-            hitReceveid(1, v);
-        }
-    }
-
-
-
-//il faut juste créer la fonction qui détécte la collision du hit et appelé ça
+    
     private void sendHit(character ennemieHitted, Vector3 hitDirection)
     {
         ennemieHitted.hitReceveid(powerOffset, hitDirection);
@@ -56,8 +63,65 @@ public class character : MonoBehaviour
 
     private void addForceByhit(int _powerOffset, Vector3 hitDirection)
     {
-        rb.AddForce(hitDirection * (((_powerOffset * _expulsionPercentage.getCharacterExpulsionPercentage()) / 20)), ForceMode2D.Force);
+        rb.AddForce(hitDirection * (((_powerOffset * _expulsionPercentage.getCharacterExpulsionPercentage()))), ForceMode2D.Force);
         Debug.Log(((_powerOffset * _expulsionPercentage.getCharacterExpulsionPercentage()) / 10));
     }
 
+    public void move()
+    {
+         Debug.Log("Move");
+    }
+
+    private void Update()
+    {
+        if(isJumping)
+        {
+            if(currentJumpTime < 0.5f)
+            {
+            currentJumpTime += Time.deltaTime;
+            rb.AddForce(Vector2.up * 500, ForceMode2D.Force);
+            }
+        }
+
+        if((rb.velocity.y != 0 && currentJumpTime > 0.15f) || (remainingJumps == 0))
+        {
+            if (checkIfGrounded())
+            {
+                isJumping = false;
+                resetJump();
+            }
+        }
+    }
+
+   public void Jump()
+{
+    if (remainingJumps > 0)
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
+        rb.AddForce(Vector2.up * 500, ForceMode2D.Impulse);
+        remainingJumps--;
+        isJumping = true;
+    }
+}
+
+    public void stopJump()
+    {
+       isJumping = false;
+       currentJumpTime = 0;
+    }
+
+    private bool checkIfGrounded()
+    {
+        float raycastDistance = 1f;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, raycastDistance, LayerMask.GetMask("Ground"));
+        return hit.collider != null;
+    }
+
+
+    private void resetJump()
+    {
+            Debug.Log("reset jump");
+            currentJumpTime = 0;
+            remainingJumps = 2;
+    }
 }
--- SmashBros/Assets/Scripts/playerController.cs
+++ SmashBros/Assets/Scripts/playerController.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    private character c;
+    private bool jump;
 
     void Start()
     {
-        
+        c = GetComponent<character>();
+    }
+    public void onMove(InputAction.CallbackContext context)
+    {
+        c.move();
     }
 
+    public void onJump(InputAction.CallbackContext context)
+    {
+
+        if(context.started)
+        {
+            c.Jump();
+        }
+        else if (context.canceled)
+        {
+            c.stopJump();
+        }
+
+    }
 }
