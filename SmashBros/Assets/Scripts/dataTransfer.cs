14665895e472831912cde6d839ce7ba1a7d57772
Loh10 <93720015+loh10@users.noreply.github.com>
2023-11-10 11:28:32 +0100

    Update dataTransfer.cs

--- SmashBros/Assets/Scripts/dataTransfer.cs
+++ SmashBros/Assets/Scripts/dataTransfer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dataTransfer
+public static class dataTransfer
 {
     public static int maxJumpHeight;
     public static float jumpTime;
